6c837d6db141abb8728ffe9e6c875173a974fc02
Asad Ullah <asad.ghumman123@gmail.com>
2024-04-16 19:22:00 +0100

    change
    change

--- face_recog_deploy.py
+++ face_recog_deploy.py
@@ -25,13 +25,20 @@ def recognize_face(clf, face_encoding):
     return clf.predict([face_encoding])[0]
 
 
-# In[10]:
+# In[12]:
 
 
 # Use the trained classifier for new predictions
 #new_image = face_recognition.load_image_file('test/adele.jpg')
 #new_face_encoding = face_recognition.face_encodings(new_image)[0]
-new_face_encoding = np.load('elon.npy')
+
+new_face_encoding = np.load('test/elon.npy')
+#new_face_encoding = np.load('test/amir.npy')
+#new_face_encoding = np.load('test/adele.npy')
+#new_face_encoding = np.load('test/alia.npy')
+#new_face_encoding = np.load('test/obama.npy')
+#new_face_encoding = np.load('test/enrique.npy')
+
 clf = load_model('face_recognition_model.pkl')
 result = recognize_face(clf, new_face_encoding)
 print(result)
