21ce4771ce0cfe87aa69e19dfae309ffeddd2d49
Asad Ullah <asad.ghumman123@gmail.com>
2024-04-16 18:38:02 +0100

    update_file
    update_file

--- face_recog_deploy.py
+++ face_recog_deploy.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
+# In[5]:
 
 
 import pickle
-import face_recognition
+import numpy as np
 
 
-# In[12]:
+# In[6]:
 
 
 # Function to load the model from a file
@@ -17,7 +17,7 @@ def load_model(model_path):
         return pickle.load(file)
 
 
-# In[13]:
+# In[7]:
 
 
 # Step 6: Model inference
@@ -25,15 +25,40 @@ def recognize_face(clf, face_encoding):
     return clf.predict([face_encoding])[0]
 
 
-# In[15]:
+# In[10]:
 
 
 # Use the trained classifier for new predictions
-new_image = face_recognition.load_image_file('test/elon.jpg')
-new_face_encoding = face_recognition.face_encodings(new_image)[0]
+#new_image = face_recognition.load_image_file('test/adele.jpg')
+#new_face_encoding = face_recognition.face_encodings(new_image)[0]
+new_face_encoding = np.load('elon.npy')
 clf = load_model('face_recognition_model.pkl')
 result = recognize_face(clf, new_face_encoding)
 print(result)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
